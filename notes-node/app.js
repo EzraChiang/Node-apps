0b32f65e13449c9e3696e851fec519dde8696bb3
Ezra Chiang <ezrachiang@Ezras-MacBook-Pro.local>
2016-12-08 00:18:45 -0800

    Work with JSON data

--- notes-node/app.js
+++ notes-node/app.js
@@ -7,7 +7,7 @@ const yargs = require('yargs');
 const notes = require('./notes');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);
 
@@ -21,7 +21,7 @@ else if (command === 'read') {
   notes.getNote(argv.title);
 }
 else if (command === 'remove') {
-  console.log('Removing note');
+  notes.removeNote(argv.title);
 }
 else {
   console.log('Cannot read command');
