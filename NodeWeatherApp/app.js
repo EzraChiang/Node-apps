46cdaecd9038ab2b52e31c2072c55cdebf09c6c9
Ezra Chiang <ezrachiang@Ezras-MacBook-Pro.local>
2016-12-09 17:46:27 -0800

    Encode user input

--- NodeWeatherApp/app.js
+++ NodeWeatherApp/app.js
@@ -1,8 +1,27 @@
 const request = require('request');
 
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address)
+
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
