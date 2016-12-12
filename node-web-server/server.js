0a4b261203c2df6920951a50adce433a50328404
Ezra Chiang <ezrachiang@Ezras-MacBook-Pro.local>
2016-12-12 00:15:18 -0800

    Create Express middleware for page maintenance

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
-
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
 });
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'maintenance page',
+    message: "We'll be right back"
+  });
+});
+//the order of app.use matters! Goes by order
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
