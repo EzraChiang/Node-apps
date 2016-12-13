698e7c1dfa823f0fad4f210d4f29b9cacd6d3825
Ezra Chiang <ezrachiang@Ezras-MacBook-Pro.local>
2016-12-12 18:32:49 -0800

    Write a test for setName

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,41 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44 but got back ${res}.`)
-  };
+  expect(res)
+    .toBe(44)
+    .toBeA('number');
+
 });
 
 it('should square two numbers', () => {
   var res = utils.square(5);
 
-  if (res !== 25) {
-    throw new Error(`Expected 25 but got back ${res}`);
-  };
+  expect(res)
+    .toBe(25)
+    .toBeA('number');
+
 });
+
+it('should expect some values', () => {
+  var user = {age: 23, DOB: '04/09/1993', favoriteMovie: 'Dark Knight', married: false};
+  utils.setName(user, 'Ezra Chiang');
+  expect(user)
+    .toInclude({
+      firstName: 'Ezra',
+      lastName: 'Chiang'
+    });
+});
+
+// it('should expect some values', () => {
+//   expect({name: "Ezra"}).toEqual({name: "Ezra"});
+//   expect([2,3,4]).toExclude(5);
+//   expect({
+//     name: "Ezra",
+//     age: "23"
+//   }).toInclude({
+//     name: "Ezra"
+//   })
+// });
