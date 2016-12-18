1f1e28799630bb2478ba20ed83eafa2f0b3c5206
Ezra Chiang <ezrachiang@Ezras-MBP.netgear.com>
2016-12-17 17:22:23 -0800

    Setup separatetest and local environment test databases

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //set up for Heroku
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
