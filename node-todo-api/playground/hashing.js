79ec28d7d619c854c53b3be85c311c72c50a2e7d
Ezra Chiang <ezrachiang@Ezras-MBP.netgear.com>
2016-12-18 14:27:58 -0800

    Hash user passwords

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,13 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
 
-var data = {
-  id: 10
-};
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abc');
+var hashedPassword = '$2a$10$wUgeBLlGHuQXJp9x.cCXtu0MpPy4./WuRS5837RHcXMnyDNdp2ri.';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// var decoded = jwt.verify(token, '123abc');
 
 //
 // var message = 'I am a message';
