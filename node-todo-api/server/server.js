6bfe6e2454f79bf25cf742f5851e9ef8f4ea7b0b
Ezra Chiang <ezrachiang@Ezras-MBP.netgear.com>
2016-12-19 02:07:18 -0800

    Convert GET /todos and POST /todos to private routes

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -15,10 +15,11 @@ const port = process.env.PORT
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
 
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -29,8 +30,10 @@ app.post('/todos', (req, res) => {
 
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
