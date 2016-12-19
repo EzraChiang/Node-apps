c47e5d89e3df93720cf9ca2a8db972fd52aed99e
Ezra Chiang <ezrachiang@Ezras-MBP.netgear.com>
2016-12-19 11:59:15 -0800

    Config JWT_SECRET

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -44,7 +44,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens.push({access, token});
 
@@ -73,7 +73,7 @@ UserSchema.statics.findByToken = function (token) {
   //try catch block stops your program and then continues
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     // return new Promise((resolve, reject) => {
     //   reject();
